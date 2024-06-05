b615c3cabe7029b6a130653144bbda7f94ffeb5b
Trip <ansxz.ansxzk@gmail.com>
2024-06-05 20:02:04 +0300

    initial commit!

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # Asybot
-Simple Discord bot written in Python using Py-Cord, with the goal of trying to make a belivable person who chats at random
+Very simple Discord bot written in Python using Py-Cord, with the goal of trying to make a belivable person who chats at random
 intervals, and remembers others' actions and general sentiment towards them.
 
-Currently, it uses a model of your choosing using [Cloudflare AI](https://ai.cloudflare.com/).
+Currently, it uses a model of your choosing using [Cloudflare AI](https://ai.cloudflare.com/), and uses a .env that follows this format:
+```
+BOT_TOKEN='# Discord Bot Token'
+CF_TOKEN='# Cloudflare AI API Token'
+CF_ACCOUNT_ID='# Found in the Cloudflare dashboard'
 
-This README is a work in progress, and will be updated as the bot is developed.
+# AI Configs:
+SYSTEM_ROLE='# This is the role the bot will play and follow'
+MODEL='# Model ID, found in Cloudflare's docs'
+```
+
+This README (and project!) are in a very early stage and are a work in progress, and will be updated and grown further as the bot is developed.
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import threading
+
 import discord
 import os
 from dotenv import load_dotenv
@@ -5,6 +7,9 @@ import requests
 
 load_dotenv()
 bot = discord.Bot()
+
+system_role = os.getenv('SYSTEM_ROLE')
+model = os.getenv('MODEL')
 api_base_url = f"https://api.cloudflare.com/client/v4/accounts/{os.getenv("CF_ACCOUNT_ID")}/ai/run/"
 headers = {"Authorization": f"Bearer {os.getenv("CF_TOKEN")}"}
 
@@ -14,28 +19,31 @@ async def on_ready():
 	print(f"Logged in as {bot.user}")
 	
 	
-def run_model(prompt) -> str:
+def run_model(prompt: str) -> str:
 	"""
 	Runs the model with the given prompt.
-	:param prompt: Should be a string of the user prompt to run the model with.
 	:return: A string of the model's response, filtered from all the json.
 	"""
 	
+	print("Firing request!")
+	
 	inputs = [
-		{"role": "system", "content": os.getenv('SYSTEM_ROLE')},
+		{"role": "system", "content": system_role},
 		{"role": "user", "content": f"{prompt}"}
 	]
 	
-	print("Fired request!")
 	prompts: dict = {"messages": inputs}
-	response = requests.post(f"{api_base_url}{os.getenv("MODEL")}", headers=headers, json=prompts)
-	if response.status_code != 200 or not response.json()["success"]:
+	response = requests.post(f"{api_base_url}{model}", headers=headers, json=prompts)
+	
+	if response.status_code != 200:  # Error handling!!!
 		print(response.json())
 		return f"An error occurred: {response.json()['errors'][0]['message']}\n Status code: {response.status_code}\n Sorry, please try again."
+	
 	reply: str = response.json()["result"]["response"]
-	if len(reply) > 2000:
+	
+	if len(reply) > 2000:  # Discord doesn't allow bots to send more than 2000 characters, apparently?
 		reply = reply[:1997] + "..."
-	print(reply)
+
 	return reply
 
 
